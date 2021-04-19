527ba71dafe96f53765af61f8e1b34dee34c5232
kat17112002 <82775290+kat17112002@users.noreply.github.com>
2021-04-19 11:07:27 +0500

    Add files via upload

---
+++ MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public int level;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void startGame()
+	{
+		Application.LoadLevel (level);
+	}
+	public void exitGame()
+	{
+		Application.Quit ();
+	}
+	//Настройки
+	public void LowQuality()
+	{
+		QualitySettings.SetQualityLevel (0,true);
+	}
+	public void MediumQuality()
+	{
+		QualitySettings.SetQualityLevel (2,true);
+	}
+	public void UltraQuality()
+	{
+		QualitySettings.SetQualityLevel (4,true);
+	}
+}
